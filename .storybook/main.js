76ddf944a405fb2403e39a98792cc9490c33a9cd
Les Peabody <lpeabody@gmail.com>
2022-01-19 16:18:23 -0500

    Add HTML preview.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@whitespace/storybook-addon-html"
   ],
   "framework": "@storybook/html",
   "core": {
